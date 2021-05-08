7ff2aeaa3c1ca17a942f9870a217c4734dbd4bff
Bruno Leite <brunomlbr@gmail.com>
2021-05-07 22:29:19 -0300

    task 1-6  mark a todo as done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,103 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( (user) => user.username === username );
+
+  if(!user) {
+    return response.status(404).json({
+      error: "Username not found! "
+    });
+  };
+
+  request.user =  user ;
+
+  return next();
 }
-
+// create new user
 app.post('/users', (request, response) => {
-  // Complete aqui
-});
+  const { name, username } = request.body ;
 
+  const userAlreadyExists = users.some( 
+    (user) =>  user.username === username
+    );
+
+  if(userAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists!"}); 
+  }
+  
+  const user = { 
+                id: uuidv4(), 
+                name, 
+                username, 
+                todos: []
+              };
+
+            
+  users.push(user);            
+
+  return response.status(201).send(user);
+
+});
+// show todo information
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+  
+  const { user } = request ;
+  
+  return response.json(user.todos);
 
+});
+// create a new todo
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+  
+  const { title, deadline } = request.body;
 
+  const { user } = request ;
+  
+  const todoTask =
+  { 
+    id: uuidv4(), 
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  };
+
+  user.todos.push(todoTask);
+
+  //return response.status(201).send(todoTask);
+  return response.status(201).json(todoTask);
+
+});
+// update a todo
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
+  const { id } = request.params;
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline) ;
+  // status padrão é 200
+  return response.json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
